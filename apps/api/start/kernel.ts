16e92ae095f58a49ebafabe9207fdbbb824f1186
Anas Mazouni <madadj4@gmail.com>
2023-09-13 21:34:56 +0200

    fix: remove broken package (adonis-autoswagger)

--- apps/api/start/kernel.ts
+++ apps/api/start/kernel.ts
@@ -23,6 +23,7 @@ import Server from '@ioc:Adonis/Core/Server'
 Server.middleware.register([
   () => import('@ioc:Adonis/Core/BodyParser'),
   () => import('@ioc:Adonis/Addons/RmbMiddleware'),
+  () => import('App/Middleware/SilentAuth'),
 ])
 
 /*
@@ -41,4 +42,6 @@ Server.middleware.register([
 | Route.get('dashboard', 'UserController.dashboard').middleware('auth')
 |
 */
-Server.middleware.registerNamed({})
+Server.middleware.registerNamed({
+  auth: () => import('App/Middleware/Auth'),
+})
