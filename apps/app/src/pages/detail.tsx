841defe869e64bf73de49e7c9d78a4a5c354b677
polymahh <otman.elkantaoui@gmail.com>
2023-09-17 00:16:21 +0100

    fixing detail page

--- apps/app/src/pages/detail.tsx
+++ apps/app/src/pages/detail.tsx
@@ -11,11 +11,11 @@ const Detail = () => {
   const type = location.pathname.split('/')[2];
 
   return (
-    <div className="pb-32 overflow-y-auto">
-      <BackButton className="absolute top-10 left-4 z-10" />
+    <div className="-mb-20 overflow-y-auto ">
+      <BackButton className="absolute z-10 top-10 left-4" />
       <img src="https://maeq-tracker.rocmine.net/assets/earth.webp" className=" h-[40vh] object-cover w-full" />
       <div className="flex flex-col gap-2 p-6">
-        <div className="flex gap-4 justify-between items-center">
+        <div className="flex items-center justify-between gap-4">
           <h1 className="text-xl font-medium">
             Detail {type} {id}
           </h1>
@@ -25,43 +25,43 @@ const Detail = () => {
         </div>
         <div className="flex justify-between">
           <p>Marrackecxcecj, Yes</p>
-          <span className="text-gray-500 text-sm">10 hours ago</span>
+          <span className="text-sm text-gray-500">10 hours ago</span>
         </div>
-        <h3 className="text-gray-500 mt-2">Description</h3>
+        <h3 className="mt-2 text-gray-500">Description</h3>
         <p className="text-justify">
           English 2/20: Hello, I am in need of warm clothing for the upcoming winter season. I'm a single mother with
           two young children, and we&quot;ve been struggling to stay warm. If anyone can help us with winter coats,
           gloves, and scarves, we would be incredibly grateful. Your support means the world to us.
         </p>
 
-        <h3 className="text-gray-500 my-2">Contact Info</h3>
+        <h3 className="my-2 text-gray-500">Contact Info</h3>
 
         <div className="flex flex-col gap-4 font-medium">
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <PhoneIcon className="w-4 h-4" />
             <a
               href="tel:+212666666666"
-              className="flex-grow underline-offset-4 decoration-dotted underline decoration-red-200"
+              className="flex-grow underline underline-offset-4 decoration-dotted decoration-red-200"
             >
               +212 6 66 66 66 66
             </a>
           </div>
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <MailIcon className="w-4 h-4" />
             <a
               href="mailto:test@stormix.co"
-              className="flex-grow underline-offset-4 decoration-dotted underline decoration-red-200"
+              className="flex-grow underline underline-offset-4 decoration-dotted decoration-red-200"
             >
               test@stormix.co
             </a>
           </div>
         </div>
 
-        <h3 className="text-gray-500 my-2">Location</h3>
+        <h3 className="my-2 text-gray-500">Location</h3>
 
         <LocationMap />
 
-        <div className="absolute px-6 bottom-0 w-full pb-8 -ml-6 z-30 bg-white pt-4">
+        <div className="absolute bottom-0 z-30 w-full px-6 pt-4 pb-8 -ml-6 bg-white">
           <div className="flex gap-4">
             <Button variant="primary" className="w-full">
               Offer assistance
