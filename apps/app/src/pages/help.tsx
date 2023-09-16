4c79e83810f93151b43bb6a4776b9150e855b8e2
polymahh <otman.elkantaoui@gmail.com>
2023-09-16 23:27:25 +0100

    feat: fix  logo and search for ease of access

--- apps/app/src/pages/help.tsx
+++ apps/app/src/pages/help.tsx
@@ -4,15 +4,16 @@ import Card from '../components/molecules/card';
 
 const Help = () => {
   return (
-    <div className="flex flex-col w-full h-full gap-4 justify-start  px-4">
-      <h1 className="font-medium text-2xl text-center">Requesting help</h1>
-
-      <SearchInput />
-      <div className="flex flex-row justify-start">
-        <FilterButton />
+    <div className="flex flex-col justify-start w-full gap-4 ">
+      <h1 className="text-2xl font-medium text-center">Requesting help</h1>
+      <div className="sticky top-0 px-4 bg-white">
+        <SearchInput />
+        <div className="flex flex-row justify-start pb-2">
+          <FilterButton />
+        </div>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 px-4">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
           <Card key={index} className="" />
         ))}
--- apps/app/src/pages/home.tsx
+++ apps/app/src/pages/home.tsx
@@ -5,8 +5,8 @@ import CarpoolingCard from '../components/molecules/carpooling-card';
 
 const Home = () => {
   return (
-    <div className="flex flex-col w-full h-full gap-4  px-4">
-      <SearchInput />
+    <div className="flex flex-col w-full gap-4 px-4">
+      <SearchInput className="sticky top-0 bg-white" />
 
       <Carousel items={[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => Card)} title="Requesting help" />
       <Carousel items={[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => Card)} title="Offering help" />
