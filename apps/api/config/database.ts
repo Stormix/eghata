076b47f9d318026bb06ef69895ab2b07a7b54c4d
Anas Mazouni <madadj4@gmail.com>
2023-09-18 19:49:07 +0200

    feat(api): help request create

--- apps/api/config/database.ts
+++ apps/api/config/database.ts
@@ -40,15 +40,15 @@ const databaseConfig: DatabaseConfig = {
         port: Env.get('PG_PORT'),
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        database: Env.get('PG_DB_NAME')
       },
       migrations: {
-        naturalSort: true,
+        naturalSort: true
       },
       healthCheck: true,
-      debug: true,
-    },
-  },
+      debug: true
+    }
+  }
 }
 
 export default databaseConfig
