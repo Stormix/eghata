35ea6347b2e3c50dc382491f48c0b73899b229cc
polymahh <otman.elkantaoui@gmail.com>
2023-09-19 16:28:45 +0100

    feat: added new svg/ fixed navbar

--- apps/app/src/components/molecules/navbar.tsx
+++ apps/app/src/components/molecules/navbar.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as CarIcon } from '@/assets/icons/car.svg';
 import { ReactComponent as HandIcon } from '@/assets/icons/hand.svg';
 import { ReactComponent as HomeIcon } from '@/assets/icons/home.svg';
 import { ReactComponent as MapIcon } from '@/assets/icons/map.svg';
-import { ReactComponent as NavBackground } from '@/assets/nav.svg';
+import { ReactComponent as NavCenter } from '@/assets/nav-center.svg';
 import { cn } from '@/lib/utils';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ActionButton from '../atoms/action-button';
@@ -25,14 +25,14 @@ const Navbar = ({ asSubmit, loading, disabled }: NavbarProps) => {
   ];
 
   return (
-    <div className="fixed bottom-0 left-0 flex w-full justify-center z-10">
+    <div className="fixed bottom-0 left-0 z-10 flex justify-center w-full">
       <ActionButton
-        className="absolute bottom-1/2 mb-4 left-1/2 -translate-x-1/2 z-30"
+        className="absolute z-30 mb-4 -translate-x-1/2 bottom-1/2 left-1/2"
         asSubmit={asSubmit}
         disabled={disabled}
         loading={loading}
       />
-      <div className="z-20 w-full flex flex-row gap-16 py-4 justify-between px-6 mb-3">
+      <div className="z-20 flex flex-row justify-center w-full px-6 py-4 mb-3 gap-28 ">
         <div className="flex gap-9">
           <HomeIcon onClick={() => navigate('/')} className={cn(...iconClasses('/'))} />
           <CarIcon onClick={() => navigate('/carpooling')} className={cn(...iconClasses('/carpooling'))} />
@@ -42,8 +42,12 @@ const Navbar = ({ asSubmit, loading, disabled }: NavbarProps) => {
           <MapIcon onClick={() => navigate('/map')} className={cn(...iconClasses('/map'))} />
         </div>
       </div>
-
-      <NavBackground className="absolute bottom-0 left-0 w-full z-10" />
+      {/* nav bar dynamic background */}
+      <div className="absolute bottom-0 flex items-end left-0 z-10 w-full h-[94px]">
+        <div className=" h-full flex-1 navbar-shadow bg-white  rounded-l-[40px]"></div>
+        <NavCenter className="z-11" />
+        <div className=" h-full flex-1  navbar-shadow bg-white  rounded-r-[40px]"></div>
+      </div>
     </div>
   );
 };
